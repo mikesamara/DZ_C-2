2b749721f76671ecce588918ecaadea2bda7ddaa
Maik <samara.maik@yandex.by>
2023-01-26 16:14:35 +0300

    add task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите трехначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int firstnumber = a % 100;
+int secondnumber = a % 10;
+int b = (firstnumber - secondnumber) / 10;
+System.Console.WriteLine(b);
