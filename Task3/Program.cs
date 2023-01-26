e37fc3e0ec6f91631add69f5672a550b954d1b29
Maik <samara.maik@yandex.by>
2023-01-26 17:15:29 +0300

    add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Веедите день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 1 && a < 6)
+{
+    System.Console.WriteLine("не выходной день");
+}
+
+else if (a > 5 && a <= 7)
+{
+    System.Console.WriteLine("Выходной день");
+}
+else 
+{
+    System.Console.WriteLine("Такого дня недели не сущесвует");
+}
