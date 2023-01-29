546d88da172fea340046acbc3d12c4df710e6c4a
Maik <samara.maik@yandex.by>
2023-01-29 12:46:39 +0300

    add task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("-->");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 99){
+      string str = Convert.ToString(a);
+     System.Console.WriteLine(str [2]);
+}
+else
+{
+    System.Console.WriteLine("Третьего числа нет");
+}
+
+
+
+
+
+
+
+
+ 
